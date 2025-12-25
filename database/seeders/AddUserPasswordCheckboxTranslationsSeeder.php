92f9f3bb75536556e7528a7e296795d56db80486
faisalkhalid09 <faisal3245.com@gmail.com>
2025-12-25 21:30:17 +0500

    Fix translation seeder column names

--- database/seeders/AddUserPasswordCheckboxTranslationsSeeder.php
+++ database/seeders/AddUserPasswordCheckboxTranslationsSeeder.php
@@ -13,49 +13,27 @@ class AddUserPasswordCheckboxTranslationsSeeder extends Seeder
     public function run(): void
     {
         $translations = [
-            // English
             [
-                'locale' => 'en',
                 'key' => 'pages.users_page.user_modal.set_password_now',
-                'value' => 'Set password for this user now',
+                'en_text' => 'Set password for this user now',
+                'fr_text' => 'Définir le mot de passe pour cet utilisateur maintenant',
+                'ar_text' => 'تعيين كلمة المرور لهذا المستخدم الآن',
             ],
             [
-                'locale' => 'en',
                 'key' => 'pages.users_page.user_modal.set_password_now_hint',
-                'value' => 'If unchecked, the user will receive an email with a link to set their own password.',
-            ],
-            // French
-            [
-                'locale' => 'fr',
-                'key' => 'pages.users_page.user_modal.set_password_now',
-                'value' => 'Définir le mot de passe pour cet utilisateur maintenant',
-            ],
-            [
-                'locale' => 'fr',
-                'key' => 'pages.users_page.user_modal.set_password_now_hint',
-                'value' => 'Si non coché, l\'utilisateur recevra un email avec un lien pour définir son propre mot de passe.',
-            ],
-            // Arabic
-            [
-                'locale' => 'ar',
-                'key' => 'pages.users_page.user_modal.set_password_now',
-                'value' => 'تعيين كلمة المرور لهذا المستخدم الآن',
-            ],
-            [
-                'locale' => 'ar',
-                'key' => 'pages.users_page.user_modal.set_password_now_hint',
-                'value' => 'إذا لم يتم التحديد، سيتلقى المستخدم بريدًا إلكترونيًا يحتوي على رابط لتعيين كلمة المرور الخاصة به.',
+                'en_text' => 'If unchecked, the user will receive an email with a link to set their own password.',
+                'fr_text' => 'Si non coché, l\'utilisateur recevra un email avec un lien pour définir son propre mot de passe.',
+                'ar_text' => 'إذا لم يتم التحديد، سيتلقى المستخدم بريدًا إلكترونيًا يحتوي على رابط لتعيين كلمة المرور الخاصة به.',
             ],
         ];
 
         foreach ($translations as $translation) {
             UiTranslation::updateOrCreate(
+                ['key' => $translation['key']],
                 [
-                    'locale' => $translation['locale'],
-                    'key' => $translation['key'],
-                ],
-                [
-                    'value' => $translation['value'],
+                    'en_text' => $translation['en_text'],
+                    'fr_text' => $translation['fr_text'],
+                    'ar_text' => $translation['ar_text'],
                 ]
             );
         }
